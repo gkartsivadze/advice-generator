e7e55c812c937bff5a4d7c9e9bb2ae682526afb2
gkartsivadze <qarcigio444@mail.ru>
2023-10-05 20:26:23 +0400

    Added data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react"
+
 function App() {
-  
+  const [adviceData, setAdviceData] = useState({
+    id: 5,
+    advice: "Brush your teeth the moment you get up."
+  });
+
+  // useEffect(() => {
+  //   fetch("https://api.adviceslip.com/advice")
+  //     .then(data => data.json())
+  //     .then(data => setAdviceData(data.slip))
+  // }, [])
+
   return (
-    <>
-    <h3>
-      Let's begin
-    </h3>
-    </>
+    <main
+      className=" min-h-screen bg-dark-grayish-blue grid place-items-center"
+    >
+      <div className=" w-1/2 h-96 p-10 bg-grayish-blue">
+        <h3>ADVICE {"#" + adviceData.id}</h3>
+        <h1 className="text-5xl text-center text-light-cyan">
+          "{adviceData.advice}"
+        </h1>
+      </div>
+    </main>
   )
 }
 
