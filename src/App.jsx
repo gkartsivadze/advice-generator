a8e94362803bf6591b24708227c672f649d31071
gkartsivadze <qarcigio444@mail.ru>
2023-10-06 12:32:25 +0400

    - Changed icons from SVG to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { PatternDividerDesktop, PatternDividerMobile } from "./assets";
 
 function App() {
   const [adviceData, setAdviceData] = useState({
@@ -16,11 +17,12 @@ function App() {
     <main
       className=" min-h-screen bg-dark-grayish-blue grid place-items-center"
     >
-      <div className=" w-1/2 h-96 p-10 bg-grayish-blue">
-        <h3>ADVICE {"#" + adviceData.id}</h3>
+      <div className="grid place-items-center gap-8 w-1/2 min-h-[5rem] p-10 bg-grayish-blue">
+        <h3 className=" text-neon-green">ADVICE {"#" + adviceData.id}</h3>
         <h1 className="text-5xl text-center text-light-cyan">
           "{adviceData.advice}"
         </h1>
+        <PatternDividerDesktop />
       </div>
     </main>
   )
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { IconDice } from "./IconDice"
+export { PatternDividerDesktop } from "./PatternDividerDesktop"
+export { PatternDividerMobile } from "./PatternDividerMobile"
